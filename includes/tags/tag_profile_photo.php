ebb442d60a99537824c1da5e755d0d1fde6f1fac
Himanshu Borah <borahhimanshu3@gmail.com>
2023-09-08 22:35:45 +0530

    Fixed

--- includes/tags/tag_profile_photo.php
+++ includes/tags/tag_profile_photo.php
@@ -22,15 +22,15 @@ class Elementor_Profile_Photo_Tag extends \Elementor\Core\DynamicTags\Data_Tag {
     public function get_value( array $options = array() )
     {
 
-		$current_user = wp_get_current_user();
+		$current_user = $this->current_user();
 	
-		if (!$current_user->ID) {
-			return []; // No user logged in
+		if(!$current_user) {
+			return [];
 		}
 	
 		// Get the user meta for 'profile_photo'
 		$meta_key = 'profile_photo_url';
-		$meta_value = get_user_meta($current_user->ID, $meta_key, true);
+		$meta_value = get_user_meta($current_user, $meta_key, true);
 	
 		if (!$meta_value) {
 			return []; // No meta value found
@@ -42,6 +42,26 @@ class Elementor_Profile_Photo_Tag extends \Elementor\Core\DynamicTags\Data_Tag {
             ]; 
     }
 
+	public function current_user() {
+	    global $post;
+
+		// Get the current user
+		$current_post = $post->ID;
+	    $current_user = get_post_meta($current_post, 'associated_user', true);
+
+		$loggedIn = wp_get_current_user();
+		if(current_user_can('administrator') && $post->post_type == 'template-manager') {
+			return $loggedIn->ID;
+
+		}
+		
+		if (!$current_user) {
+			echo $this->get_title();
+			return;
+		}
+
+		return $current_user;
+	}
     // public function get_value( array $options = [] ) {
 	// 	// Get the current user
 	// 	$current_user = wp_get_current_user();
