280f7aeb47f7e1f808815d77bd6b181fa38f6258
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-13 17:25:26 +0530

    ...

--- includes/extension/class-plugin-name-extension.php
+++ includes/extension/class-plugin-name-extension.php
@@ -1,4 +1,4 @@
 <?php
 
-require_once plugin_dir_path( dirname( __FILE__ ) ) . 'class-plugin-name-form-manager.php';
-require_once plugin_dir_path( dirname( __FILE__ ) ) . 'class-plugin-name-dashboard.php';
+require_once plugin_dir_path( dirname( __FILE__ ) ) . '/extension/class-plugin-name-form-manager.php';
+require_once plugin_dir_path( dirname( __FILE__ ) ) . '/extension/class-plugin-name-dashboard.php';
