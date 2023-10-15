71ea6f0188babf97593dcb1d2c8399a449f8bc80
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-15 11:45:49 +0530

    ...

--- includes/class-plugin-name-builder.php
+++ includes/class-plugin-name-builder.php
@@ -333,7 +333,7 @@ class Plugin_Name_Builder {
                             
                             </button>
 
-                            <div class="flex flex-row items-center gap-2 uppercase font-semibold text-sm sm:text-[1rem] mb-10">
+                            <div class="flex flex-row items-center gap-2 mb-10 text-sm font-semibold uppercase sm:text-base">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="text-[#f1441e] text-[14px]" fill="currentColor"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
                                 <span>You're adding a new social icon</span>
                             </div>
@@ -403,7 +403,7 @@ class Plugin_Name_Builder {
                                 <div id="editionForm" x-show="link.isEditing">
                                     <div class="p-5 mt-5">
 
-                                        <div class="flex flex-row items-center gap-2 uppercase font-semibold text-sm sm:text-[1rem] mb-10">
+                                        <div class="flex flex-row items-center gap-2 mb-10 text-sm font-semibold uppercase sm:text-base">
                                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="text-[#f1441e] text-[14px]" fill="currentColor"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
                                             <span>You're editing your social icon</span>
                                         </div>
@@ -482,11 +482,10 @@ class Plugin_Name_Builder {
                     <div x-show="showAddNewLinkForm">
                         <div class="relative p-10 my-5 bg-gray-50 m-0 sm:m-5 border-solid rounded-[10px] border-[1px] border-[#F1441E]">
                             <button @click.prevent="showAddNewLinkForm = false" class="absolute text-gray-700 border-0 cursor-pointer top-5 right-5 bg-inherit">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 448 512" fill="currentColor"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>                           
-                            
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 448 512" fill="currentColor"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>                                               
                             </button>
                             
-                            <div class="flex flex-row items-center gap-2 uppercase font-semibold text-sm sm:text-[1rem] mb-10">
+                            <div class="flex flex-row items-center gap-2 mb-10 text-xs font-semibold uppercase sm:text-sm md:text-base">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="text-[#f1441e] text-[14px]" fill="currentColor"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
                                 <span>You're adding a new link</span>
                             </div>
@@ -633,7 +632,7 @@ class Plugin_Name_Builder {
                                     x-show="link.isEditing"
                                 >
                                     <div class="p-5 mt-5 ">
-                                        <div class="flex flex-row items-center gap-2 uppercase font-semibold text-sm sm:text-[1rem] mb-10">
+                                        <div class="flex flex-row items-center gap-2 mb-10 text-sm font-semibold uppercase sm:text-base">
                                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="text-[#f1441e] text-[14px]" fill="currentColor"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/></svg>
                                             <span>You're editing your link</span>
                                         </div>
