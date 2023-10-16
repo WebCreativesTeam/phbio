2cecee67a50b0da776127d3b4d2585a049b4535d
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-16 17:48:45 +0530

    ...

--- includes/class-plugin-name-dashboard.php
+++ includes/class-plugin-name-dashboard.php
@@ -559,10 +559,14 @@ class Plugin_Name_Dashboard {
                     
                     // Hidden Tables
                     echo "<div class='hidden'>";
+                    echo "<div class='table-wrapper'>";     
                     echo do_shortcode('[wpdatatable id=7 var1=' . $post->ID . ' var2=' . $date_from . ' var3=' . $date_to . ']');
+                    echo "</div>";
                     echo Plugin_Name_Utilities::is_empty_table(7);
                    
+                    echo "<div class='table-wrapper'>";     
                     echo do_shortcode('[wpdatatable id=15 var1=' . $date_from . ' var2=' . $date_to . ' var3=' . $user_id . ']');
+                    echo "</div>";
                     echo Plugin_Name_Utilities::is_empty_table(15);
                    
                     echo "</div>";
@@ -601,10 +605,15 @@ class Plugin_Name_Dashboard {
                  
                 // Hidden Tables
                 echo "<div class='hidden'>";
+
+                echo "<div class='table-wrapper'>";     
                 echo do_shortcode('[wpdatatable id=7 var1=' . $post->ID . ' var2=1970-01-01 var3=' . date("Y-m-d") . ']');
+                echo "</div>";
                 echo Plugin_Name_Utilities::is_empty_table(7);
 
+                echo "<div class='table-wrapper'>";     
                 echo do_shortcode('[wpdatatable id=15 var1=1970-01-01 var2=' . date("Y-m-d") . ' var3=' . $user_id . ']');
+                echo "</div>";
                 echo Plugin_Name_Utilities::is_empty_table(15);
 
                 echo "</div>";
