ab1005c56da4a89b8e6d03a35828c95a192aebfa
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-16 19:56:24 +0530

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,11 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   corePlugins: {
     preflight: false,
