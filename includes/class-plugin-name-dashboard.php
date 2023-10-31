55425f43c92230adcddda18a511fee6c1038cd48
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-31 09:06:01 +0530

    ...

--- includes/class-plugin-name-dashboard.php
+++ includes/class-plugin-name-dashboard.php
@@ -93,27 +93,28 @@ class Plugin_Name_Dashboard {
         });
         $watch('editMode', (newValue) => {
             if (newValue === true) {
-                var iframe = document.querySelector('.iframe-container' + ' iframe');
-                var loader = document.querySelector('.iframe-container' + ' .loaad');
-
-              
+                var iframe = document.querySelector('.iframe-container iframe');
+                var loader = document.querySelector('.iframe-container .loaad');      
 
                 if (!iframe || !loader) {
                     console.error('Iframe or loader not found!');
                     return;
-                }
-
-                var reloadInterval;
+                }      
                 console.log(iframe);
                 iframe.onload = function () {
                     iframe.style.display = 'block';
                     loader.style.display = 'none';
-                    iframe.style.height =
-                    iframe.contentWindow.document.body.scrollHeight + 40 + 'px';
-
-                    // Clear the interval once the iframe is loaded
-                    clearInterval(reloadInterval);
+                    try {
+                        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 40 + 'px';
+                    } catch (error) {
+                        console.error('Unable to access iframe content:', error);
+                    }
                 };
+
+                // Trigger onload if iframe is already loaded
+                if (iframe.contentWindow.document.readyState === 'complete') {
+                    iframe.onload();
+                }
             }
         });
         $watch('showSettings', () => saveState());
