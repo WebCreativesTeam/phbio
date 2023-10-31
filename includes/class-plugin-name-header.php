936f6e27309a0142ee636482629deadc8629cc12
Himanshu Borah <borahhimanshu3@gmail.com>
2023-10-31 07:53:16 +0530

    ...

--- includes/class-plugin-name-header.php
+++ includes/class-plugin-name-header.php
@@ -6,7 +6,7 @@
     <div class="box-border w-full max-w-screen-xl px-4 py-4 mx-auto text-white sm:px-6 sm:py-4">
       <div class="flex flex-wrap items-center justify-between gap-0 sm:gap-5 md:flex-nowrap md:justify-normal md:gap-12 lg:gap-24">
         <img  class="md:order-1 w-[180px] sm:w-[225px]" src ="<?php echo $logo_img; ?>"/>
-        <a href="https://produchertz.com/" target="_blank" class="text-white flex items-center h-fit md:bg-[#F1441E] p-1 px-2 md:p-2 rounded-md gap-2 md:order-3 hover:text-white">
+        <a href="https://produchertz.com/my-account" target="_blank" class="text-white flex items-center h-fit md:bg-[#F1441E] p-1 px-2 md:p-2 rounded-md gap-2 md:order-3 hover:text-white">
           <span class="md:order-2">My Account</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-[#F1441E]  md:fill-white md:order-1 md:w-4 md:h-4"
             viewBox="0 0 512 512">
