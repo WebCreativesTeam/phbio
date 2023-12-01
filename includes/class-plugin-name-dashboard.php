e8a0a7fc878a7390441910abba9494001ddbe519
Himanshu Borah <borahhimanshu3@gmail.com>
2023-12-01 11:43:54 +0530

    ..

--- includes/class-plugin-name-dashboard.php
+++ includes/class-plugin-name-dashboard.php
@@ -291,7 +291,7 @@ class Plugin_Name_Dashboard {
 
                     echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Performing Links</div>";
                     echo "<div class='table-wrapper'>";     
-                        echo do_shortcode('[wpdatatable id=15 var2=' . $date_from . ' var3=' . $date_to . ']');
+                        echo do_shortcode('[wpdatatable id=16 var2=' . $date_from . ' var3=' . $date_to . ']');
                     echo "</div>";
                     // echo Plugin_Name_Utilities::is_empty_table(15);
 
@@ -361,7 +361,7 @@ class Plugin_Name_Dashboard {
 
                 echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Performing Links</div>";
                 echo "<div class='table-wrapper'>";        
-                    echo do_shortcode('[wpdatatable id=15 var2=' . date("Y-m-d") . ' var3=' . date("Y-m-d") . ']'); 
+                    echo do_shortcode('[wpdatatable id=16 var2=' . date("Y-m-d") . ' var3=' . date("Y-m-d") . ']'); 
                 echo "</div>";
                 // echo Plugin_Name_Utilities::is_empty_table(15);
                 
