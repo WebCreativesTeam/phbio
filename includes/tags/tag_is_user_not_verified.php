7147b2debf347a4d5c7849f423be33810cb28ec7
Himanshu Borah <borahhimanshu3@gmail.com>
2023-12-18 18:20:39 +0530

    ...

--- includes/tags/tag_is_user_not_verified.php
+++ includes/tags/tag_is_user_not_verified.php
@@ -20,6 +20,7 @@ class Elementor_Is_User_Not_Verified extends \Elementor\Core\DynamicTags\Tag {
 		return [ \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY ];
 	}
 	public function render() {
+		echo "testinggg";
 		$user = $this->current_user();
 		if( !in_array( 'um_free-verified', (array) $user->roles ) && !in_array( 'um_pro-verified', (array) $user->roles )) {
 			if ( in_array( 'um_free-member', (array) $user->roles ) || in_array( 'um_pro-member', (array) $user->roles )  ) {
