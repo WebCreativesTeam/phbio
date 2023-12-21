3fd8f7dc76a1bd2213ee55b2e2798b5c954853d6
Himanshu Borah <borahhimanshu3@gmail.com>
2023-12-21 10:57:01 +0530

    ...

--- includes/extension/class-plugin-name-dashboard.php
+++ includes/extension/class-plugin-name-dashboard.php
@@ -871,6 +871,30 @@ public function component__range_picker() { ?>
                         echo Plugin_Name_Utilities::is_empty_chart('area', 25, 13);
                     }
 
+                    if(Plugin_Name_Utilities::is_full_version($user_id)) {
+                        echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Locations</div>";
+                        
+                        echo "<div class='chart-wraper'>";        
+                        echo do_shortcode('[wpdatachart id=15]');
+                        echo "</div>";
+                        echo Plugin_Name_Utilities::is_empty_chart('pie', 13, 15);
+
+                    } else {
+                        echo "<div class='analytics-label-wraper'>";
+                        echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Locations</div>";
+                        echo "<div><a class='py-2 unlock-more' href='/upgrade' target='_blank'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 448 512' fill='currentColor'><path d='M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z'></path></svg>Unlock More</a></div>";
+                        echo "</div>";
+
+                        echo "<div class='flex justify-center chart-wraper'>";     
+                        ?>   
+                        <?php $location_img = plugin_dir_url( __FILE__ ) . 'img/location.jpg'; ?>
+                        <div class="bg-cover bg-center w-[250px] h-[250px] md:w-[400px] md:h-[400px] max-w-full" style="background-image: url('<?php echo $location_img; ?>');">
+                                <div class="text-2xl md:text-4xl flex justify-center items-center w-[250px] h-[250px] md:w-[400px] md:h-[400px] bg-white bg-opacity-80"> Sample Data</div>
+                        </div>
+    
+                        <?php echo "</div>";
+
+                    }
                     
                    
                     
@@ -885,6 +909,11 @@ public function component__range_picker() { ?>
                     echo do_shortcode('[wpdatatable id=26 var1=' . $child_post_ids[0] . ' var2=' . $child_post_ids[1] . ' var3=' . $date_from . ' var4=' . $date_to . ']');
                     echo "</div>";
                     echo Plugin_Name_Utilities::is_empty_table(26);
+
+                    echo "<div class='table-wrapper'>";     
+                    echo do_shortcode('[wpdatatable id=13 var1=' . $child_post_ids[0] . ' var2=' . $child_post_ids[1] . ' var3=' . $date_from . ' var4=' . $date_to . ']');
+                    echo "</div>";
+                    echo Plugin_Name_Utilities::is_empty_table(13);
                     
                     // echo "<div class='table-wrapper'>";     
                     // echo do_shortcode('[wpdatatable id=22 var1=' . $post->ID . ' var2=' . $date_from . ' var3=' . $date_to . ']');
@@ -941,6 +970,11 @@ public function component__range_picker() { ?>
                 echo "</div>";
                 echo Plugin_Name_Utilities::is_empty_table(26);
 
+                echo "<div class='table-wrapper'>";     
+                echo do_shortcode('[wpdatatable id=13 var1=' . $child_post_ids[0] . ' var2=' . $child_post_ids[1] . ' var3=' . date("Y-m-d") . ' var4=' . date("Y-m-d") . ']');
+                echo "</div>";
+                echo Plugin_Name_Utilities::is_empty_table(13);
+
                 // echo "<div class='table-wrapper'>";     
                 // echo do_shortcode('[wpdatatable id=7 var1=' . $post->ID . ' var2=' . date("Y-m-d") . ' var3=' . date("Y-m-d") . ']');
                 // echo "</div>";
@@ -957,7 +991,29 @@ public function component__range_picker() { ?>
                
 
 
-               
+                if(Plugin_Name_Utilities::is_full_version($user_id)) {
+                    echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Locations</div>";
+                    echo "<div class='chart-wraper'>";        
+                    echo do_shortcode('[wpdatachart id=15]');
+                    echo "</div>";
+                    echo Plugin_Name_Utilities::is_empty_chart('pie', 13, 15);
+                    
+                } else {
+                    echo "<div class='analytics-label-wraper'>";
+                    echo "<div class='analytics-label input-label'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512' class='heading-icon' fill='currentColor'><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z'></path></svg> Top Locations</div>";
+                    echo "<div><a class='py-2 unlock-more' href='/upgrade' target='_blank'><svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 448 512' fill='currentColor'><path d='M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z'></path></svg>Unlock More</a></div>";
+                    echo "</div>";
+
+                    echo "<div class='flex justify-center chart-wraper'>";     
+                    ?>   
+                    <?php $location_img = plugin_dir_url( __FILE__ ) . 'img/location.jpg'; ?>
+                    <div class="bg-cover bg-center w-[250px] h-[250px] md:w-[400px] md:h-[400px] max-w-full" style="background-image: url('<?php echo $location_img; ?>');">
+                            <div class="text-2xl md:text-4xl flex justify-center items-center w-[250px] h-[250px] md:w-[400px] md:h-[400px] bg-white bg-opacity-80"> Sample Data</div>
+                    </div>
+
+                    <?php echo "</div>";
+                    
+                }
                 
                
             }
