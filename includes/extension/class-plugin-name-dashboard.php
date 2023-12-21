1e386a95201a1828a8dbed90e082ba950a99e6d1
Himanshu Borah <borahhimanshu3@gmail.com>
2023-12-21 09:27:38 +0530

    ...

--- includes/extension/class-plugin-name-dashboard.php
+++ includes/extension/class-plugin-name-dashboard.php
@@ -821,6 +821,7 @@ public function component__range_picker() { ?>
           
             <?php
             $title = get_user_meta($user_id, 'username', true);
+            echo $title;
             $post = get_page_by_path( $title, OBJECT, 'hb-user-profile' );
             echo $post->ID;
 
