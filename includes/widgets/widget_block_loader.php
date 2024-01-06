b142012bf808e3b77f07158f0425cc04d27fde85
Himanshu Borah <borahhimanshu3@gmail.com>
2024-01-06 10:58:22 +0530

    ...

--- includes/widgets/widget_block_loader.php
+++ includes/widgets/widget_block_loader.php
@@ -461,9 +461,24 @@ class Elementor_Block_Loader_Widget extends \Elementor\Widget_Base {
         echo "</textarea>";
     }
     private function render_input_oembed($value) {
-        echo "<div>";
-        $GLOBALS['wp_embed']->run_shortcode( $value );
-        echo "</div>"
+        preg_match('/src="(.+?)"/', $value, $matches);
+        $src = $matches[1];
+
+        // Add extra parameters to src and replace HTML.
+        $params = array(
+            'controls'  => 0,
+            'hd'        => 1,
+            'autohide'  => 1
+        );
+        $new_src = add_query_arg($params, $src);
+        $iframe = str_replace($src, $new_src, $iframe);
+
+        // Add extra attributes to iframe HTML.
+        $attributes = 'frameborder="0"';
+        $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+        // Display customized HTML.
+        echo $iframe;
     }
     
     
