cbba3f8cb8a603400478745643ac5017a5cd23c0
Himanshu Borah <borahhimanshu3@gmail.com>
2024-01-08 11:04:59 +0530

    ...

--- admin/class-plugin-name-admin.php
+++ admin/class-plugin-name-admin.php
@@ -477,18 +477,26 @@ class Plugin_Name_Admin {
 		
 	}
 	function redirect_draft_child_pages() {
-		if (is_page() && !is_admin()) {
+		if (is_singular('hb-user-pkit')) {
 			global $post;
-			
-        $is_child_page = is_a($post, 'WP_Post') && $post->post_parent;
-			
-			// Check if the page is a draft or child of a draft page
-			if ($is_child_page && get_post_status($post->ID) != 'publish') {
-				// Perform a redirect or display a message
-				wp_redirect(home_url()); // Redirect to home page
+			$is_child_page = is_a($post, 'WP_Post') && $post->post_parent;
+
+			if ($is_child_page) { // Check if it's a parent post
+				wp_redirect(home_url()); // Redirect to homepage or any other page
 				exit;
 			}
 		}
+		// if (is_page() && !is_admin()) {
+		// 	global $post;
+			
+			
+		// 	// Check if the page is a draft or child of a draft page
+		// 	if ($is_child_page && get_post_status($post->ID) != 'publish') {
+		// 		// Perform a redirect or display a message
+		// 		wp_redirect(home_url()); // Redirect to home page
+		// 		exit;
+		// 	}
+		// }
 	}
 	
 	
