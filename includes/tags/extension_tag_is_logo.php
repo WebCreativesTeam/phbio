fe0e974900c347df0d8bbb1b56daf91f316aeeab
Himanshu Borah <borahhimanshu3@gmail.com>
2024-01-08 23:00:40 +0530

    ....

--- includes/tags/extension_tag_is_logo.php
+++ includes/tags/extension_tag_is_logo.php
@@ -19,28 +19,34 @@ class Elementor_Ext_Is_Logo_Tag extends \Elementor\Core\DynamicTags\Tag {
 	}
     public function render() {
 		// Get the current user
-		$current_user = wp_get_current_user();
-	
-		if (!$current_user->ID) {
-			echo 'No user logged in';
-			return;
-		}
-	
+		$current_user = $this->current_user();
+
 		// Get the user meta for 'tag-name'
 		$meta_key = 'pkit_logo';
 		$meta_value = get_user_meta($current_user->ID, $meta_key, true);
 	
-		if (!$meta_value) {
-			echo 'No meta value found for ' . $meta_key;
-			return;
-		}
-	
+		
 		$isLogo = '';
-		if( $meta_value === 'yes' ) {
+		if(!$meta_value || $meta_value === 'yes' ) {
 			$isLogo = 'ph_logo_hidden';
 		}
 
 		echo $isLogo;
+	}
+
+	public function current_user() {
+	    global $post;
+
+		$parent_id = $post->post_parent;
+		
+	    $current_user = get_post_meta($parent_id, 'associated_pkit_user', true);
+
+		$loggedIn = wp_get_current_user();
+		if(current_user_can('administrator') && $post->post_type == 'pkit-template') {
+			return $loggedIn->ID;
+		}
+
+		return $current_user;
 	}
 	
 	
