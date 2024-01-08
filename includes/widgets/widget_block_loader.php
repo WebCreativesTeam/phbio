f7611308ebbe6babf13f50520f91ddd50929495f
Himanshu Borah <borahhimanshu3@gmail.com>
2024-01-08 12:44:16 +0530

    ...

--- includes/widgets/widget_block_loader.php
+++ includes/widgets/widget_block_loader.php
@@ -474,9 +474,28 @@ class Elementor_Block_Loader_Widget extends \Elementor\Widget_Base {
         }
         echo "</textarea>";
     }
-    private function render_input_oembed($value) {
+    private function render_input_oembed($iframe) {
+        
+        // Use preg_match to find iframe src.
+        preg_match('/src="(.+?)"/', $iframe, $matches);
+        $src = $matches[1];
+
+        // Add extra parameters to src and replace HTML.
+        $params = array(
+            'controls'  => 0,
+            'hd'        => 1,
+            'autohide'  => 1
+        );
+        $new_src = add_query_arg($params, $src);
+        $iframe = str_replace($src, $new_src, $iframe);
+
+        // Add extra attributes to iframe HTML.
+        $attributes = 'frameborder="0"';
+        $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+        // Display customized HTML.
         echo "<div class='embed-container'>";
-        print_r($value);
+        echo $iframe;
         echo "</div>";
     }
     
