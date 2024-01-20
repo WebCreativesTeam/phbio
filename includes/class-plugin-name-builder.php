c64c827e4c0bb25cfe329ce0c2d52f2dae36b13d
Himanshu Borah <borahhimanshu3@gmail.com>
2024-01-20 14:07:02 +0530

    ...

--- includes/class-plugin-name-builder.php
+++ includes/class-plugin-name-builder.php
@@ -516,9 +516,7 @@ class Plugin_Name_Builder {
                                 echo '<div class="warning-message"><svg xmlns="http://www.w3.org/2000/svg" class="warning-icon" width="24" height="24" viewBox="0 0 448 512" fill="currentColor"><path d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path></svg><span>' . self::ERROR_LINK_SCHEDULING . '</span></div>';
 
                             } else { ?>
-                            <?php 
-                            echo date("Y-m-d H:i:s");
-                             ?>
+                            
                             <div class="my-3 text-sm sm:text-[15px]">
                                 <label>
                                     <input type="checkbox" x-model="newLink.isScheduled">
@@ -535,12 +533,12 @@ class Plugin_Name_Builder {
                             <div class="flex flex-col w-full gap-5 my-5 md:gap-7 md:flex-row" >
                                 <div class="flex flex-col items-baseline gap-1 md:gap-3 md:flex-row md:w-fit" x-show="newLink.isScheduled">
                                     <label class="w-full input-label md:text-sm max-w-fit"> Start Time</label>
-                                    <input class="schedule_time" class="w-full md:max-w-fit" type="datetime-local" x-model="newLink.start_time" value="<?php echo date("Y-m-d\TH:i:s"); ?>">             
+                                    <input class="schedule_time" class="w-full md:max-w-fit" type="datetime-local" x-model="newLink.start_time">             
                                 </div>
         
                                 <div class="flex flex-col items-baseline gap-1 md:gap-3 md:flex-row md:w-fit" x-show="newLink.isEndScheduled">
                                     <label class="w-full input-label md:text-sm max-w-fit"> End Time</label>
-                                    <input class="schedule_time" class="w-full md:max-w-fit" type="datetime-local" x-model="newLink.end_time" value="<?php echo date("Y-m-d\TH:i:s"); ?>">
+                                    <input class="schedule_time" class="w-full md:max-w-fit" type="datetime-local" x-model="newLink.end_time">
                                 </div>
                             </div>
                             
@@ -690,12 +688,12 @@ class Plugin_Name_Builder {
                                         <div class="flex flex-col w-full gap-5 my-5 md:gap-7 md:flex-row">
                                             <div class="flex flex-col items-baseline gap-1 md:gap-3 md:flex-row md:w-fit" x-show="link.isScheduled">
                                                 <label class="w-full input-label md:text-sm max-w-fit"> Start Time</label>
-                                                <input type="datetime-local" class="w-full md:max-w-fit" x-model="link.start_time" value="<?php echo date("Y-m-d\TH:i"); ?>">             
+                                                <input type="datetime-local" class="w-full md:max-w-fit" x-model="link.start_time">             
                                             </div>
 
                                             <div class="flex flex-col items-baseline gap-1 md:gap-3 md:flex-row md:w-fit" x-show="link.isEndScheduled">
                                                 <label class="w-full input-label md:text-sm max-w-fit"> End Time</label>
-                                                <input type="datetime-local" class="w-full md:max-w-fit" x-model="link.end_time" value="<?php echo date("Y-m-d\TH:i"); ?>">
+                                                <input type="datetime-local" class="w-full md:max-w-fit" x-model="link.end_time">
                                             </div>
                                         </div>
                                         
