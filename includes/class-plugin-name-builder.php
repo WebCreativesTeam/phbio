1870cf51e8e520297f85a317186fd6ae66b033b1
Himanshu Borah <borahhimanshu3@gmail.com>
2024-02-27 12:47:41 +0530

    ...

--- includes/class-plugin-name-builder.php
+++ includes/class-plugin-name-builder.php
@@ -3,11 +3,11 @@
 class Plugin_Name_Builder {
     
     const ERROR_MSG= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>Unlock this feature instantly by <span class='text-[#F1441E] font-bold'>Going PRO</span>.</a>";
-    const ERROR_MSG_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>Unlock this feature instantly. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
+    const ERROR_MSG_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank' rel='nofollow'>Unlock this feature instantly. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
     const ERROR_MAX_LINK_MSG= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>You have hit the link limit. <span class='text-[#F1441E] font-bold'>Upgrade Now</span>.</a>";
-    const ERROR_MAX_LINK_MSG_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>You have hit the link limit. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
+    const ERROR_MAX_LINK_MSG_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank' rel='nofollow'>You have hit the link limit. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
     const ERROR_LINK_SCHEDULING= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>Unlock links scheduling option. <span class='text-[#F1441E] font-bold'>Go PRO now</span>!</a>";
-    const ERROR_LINK_SCHEDULING_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank'>Unlock links scheduling option. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
+    const ERROR_LINK_SCHEDULING_RENEW= "<a href='/upgrade' class='text-gray-700 no-underline font-semi-bold' target='_blank' rel='nofollow'>Unlock links scheduling option. <span class='text-[#F1441E] font-bold'>Renew now</span>!</a>";
     
     public static function get_error_message($user_id) {
         if (Plugin_Name_Utilities::is_user_suspended_notice($user_id)) {
--- includes/class-plugin-name-utils.php
+++ includes/class-plugin-name-utils.php
@@ -22,7 +22,7 @@ class Plugin_Name_Utilities {
             return '<div class="warning-message">
             <svg xmlns="http://www.w3.org/2000/svg" class="warning-icon" viewBox="0 0 24 24" fill="currentColor"><path d="M12,2A10,10,0,1,0,22,12,10.01114,10.01114,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8.00917,8.00917,0,0,1,12,20Zm0-8.5a1,1,0,0,0-1,1v3a1,1,0,0,0,2,0v-3A1,1,0,0,0,12,11.5Zm0-4a1.25,1.25,0,1,0,1.25,1.25A1.25,1.25,0,0,0,12,7.5Z"></path></svg>
             <span>Your
-            subscription has expired, but don\'t worry! We\'ll keep all your links live for <b>' . self::get_remaining_hours_for_user_backup($user_id) . '</b>. <a href="/my-account/subscription" class="font-semibold no-underline text-[#F1441E]" target="_blank">Renew now</a> to keep
+            subscription has expired, but don\'t worry! We\'ll keep all your links live for <b>' . self::get_remaining_hours_for_user_backup($user_id) . '</b>. <a href="/my-account/subscription" class="font-semibold no-underline text-[#F1441E]" target="_blank" rel="nofollow">Renew now</a> to keep
             them active. </span></div>';
         } else {
             return '';
